43ce97727a5191afd81fbd9f30a9311c3ff0f6da
Juan Varner <juan.varner@prodigious.com>
2020-10-22 19:25:15 -0500

    Created a Base Project Template and readme instructions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Sitecore-Base
 Sitecore Base Project
+
+Publish Instructions
+1-Back up a copy of your sitecore instance folder
+2-Backup a copy of web.config File in this project
+3-Copy web.config sitecore content and then paste it to web.config in this project
+4-Go to the back-up file for web.config of this project and copy The System.Web.Mvc dependencyAssembly oldVersionValue
+5-Paste that value in the current  web.config System.Web.Mvc -> dependencyAssembly -> oldVersionValue
+6-Do the same for asemblies such as Newtonsoft.Json
+7-In Visual Studio publish the solution as a Folder Target, the root path will be the same where the sitecore web.config (Step 1) is located  
+
+Folowed this tutorial: https://www.youtube.com/watch?v=JWCGILTZzLs
---
+++ Sitecore-Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore_Base")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sitecore_Base")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a126ad40-f13b-4631-9811-d3d5274b4ead")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
